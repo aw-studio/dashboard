c50c8c7f00c1e7d4e68c14a4a09f53684c2b1cfe
Gerrit Hoffschulte <gerrit@Gerrits-iMac-2.fritz.box>
2020-06-26 14:17:36 +0200

    wip

--- views/app.blade.php
+++ views/app.blade.php
@@ -5,8 +5,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+
+    <x-styles/>
+    <livewire:styles/>
+
+    @yield('head')
+    
 </head>
 <body>
+
+    @yield('app')
+
+    <x-scripts/>
+    <livewire:scripts/>
     
 </body>
 </html>
